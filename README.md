ee3469786d4e6767813dccfc732712829b67e8cb
sa46lll <9812smh@naver.com>
2022-01-30 21:59:37 +0900

    chore: 버전 업그레이드

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Springboot-AWS
+
+
+> 테스트 코드
+- Junit assertThat
+- assertj assertThat
+    - CoreMatchers와 달리 추가적으로 라이브러리가 필요하지 않다.
+    - 자동완성이 좀 더 확실하게 지원된다.
+
--- src/test/java/com/sall/book/springbook/web/HelloControllerTest.java
+++ src/test/java/com/sall/book/springbook/web/HelloControllerTest.java
@@ -1,16 +1,17 @@
 package com.sall.book.springbook.web;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = HelloController.class) // 컨트롤러만 사용하기 때문에 선언함
 public class HelloControllerTest {
 
