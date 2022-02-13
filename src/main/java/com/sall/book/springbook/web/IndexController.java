7bf9988448b8d39f91f6b9e65e752b81ed0c4120
sa46lll <9812smh@naver.com>
2022-02-13 12:54:35 +0900

    feat: 등록 기능 추가

--- src/main/java/com/sall/book/springbook/web/IndexController.java
+++ src/main/java/com/sall/book/springbook/web/IndexController.java
@@ -1,8 +1,10 @@
 package com.sall.book.springbook.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
